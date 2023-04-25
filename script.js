cebeafb3b9fb5d90abeb51063c7cefc2c8aca273
Joshy Merki <joshy@morntag.com>
2023-04-25 14:56:49 +0200

    changed the Won Menu to Fullscreen

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 // Sellecting the HTML Elements
+const mainHTML = document.querySelector('main');
 const player0HTML = document.querySelector('.player--0');
 const player1HTML = document.querySelector('.player--1');
 const score0HTML = document.getElementById('score--0');
@@ -16,18 +17,15 @@ let playerActiv = 0;
 let scores = [0, 0];
 let currentScore = 0;
 let win = false;
+let numberToWin = 5;
 
 // Function to reset all the Var and HTML
 const resetAll = function () {
-  if (win) {
-    document
-      .querySelector(`.player--winner`)
-      .classList.remove('player--winner');
-    win = false;
-  }
   scores = [0, 0];
   score0HTML.textContent = scores[0];
   score1HTML.textContent = scores[1];
+  currentScore0HTML.textContent = 0;
+  currentScore1HTML.textContent = 0;
   diceHTML.classList.add('hidden');
 };
 
@@ -40,29 +38,25 @@ const switchPlayer = function () {
   player1HTML.classList.toggle('player--active');
 };
 
-//Function to hold the Score
-const holdScore = function () {
-  if (!win) {
-    scores[playerActiv] += currentScore;
-    document.getElementById(`score--${playerActiv}`).textContent =
-      scores[playerActiv];
-
-    if (scores[playerActiv] >= 20) {
-      wonGame();
-    } else {
-      switchPlayer();
-    }
-  }
-};
-
 //Function to Reset the Game
 const resetGame = function () {
+  if (win) {
+    win = false;
+    document
+      .querySelector(`.player--winner`)
+      .classList.remove('player--winner');
+    mainHTML.classList.remove('player-win');
+    document.querySelector(
+      `.player--${playerActiv} .current-label`
+    ).textContent = 'CURRENT';
+  }
   currentScore = 0;
   document.getElementById(`current--${playerActiv}`).textContent = currentScore;
   playerActiv = 0;
   player0HTML.classList.add('player--active');
   player1HTML.classList.remove('player--active');
-
+  btnNewRoll.classList.remove('hidden');
+  btnHold.classList.remove('hidden');
   resetAll();
 };
 
@@ -72,10 +66,36 @@ const wonGame = function () {
   document
     .querySelector(`.player--${playerActiv}`)
     .classList.add('player--winner');
+  mainHTML.classList.add('player-win');
   document
     .querySelector(`.player--${playerActiv}`)
     .classList.remove('player--active');
+
+  //Change Texts
+  document.getElementById(`score--${playerActiv}`).textContent = 'WON';
+  document.querySelector(`.player--${playerActiv} .current-label`).textContent =
+    'Score';
+  document.querySelector(`.player--${playerActiv} .current-score`).textContent =
+    scores[playerActiv];
+  //Hide Buttons
   diceHTML.classList.add('hidden');
+  btnNewRoll.classList.add('hidden');
+  btnHold.classList.add('hidden');
+};
+
+//Function to hold the Score
+const holdScore = function () {
+  if (!win) {
+    scores[playerActiv] += currentScore;
+    document.getElementById(`score--${playerActiv}`).textContent =
+      scores[playerActiv];
+
+    if (scores[playerActiv] >= numberToWin) {
+      wonGame();
+    } else {
+      switchPlayer();
+    }
+  }
 };
 
 // Roling the Dice function
