110038d5e934d442206adfdf5eaa497c2df9289e
Joshy Merki <joshy@morntag.com>
2023-04-25 14:12:23 +0200

    finished game Logic add Hold and New Game Logic

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 // Sellecting the HTML Elements
+const player0HTML = document.querySelector('.player--0');
+const player1HTML = document.querySelector('.player--1');
 const score0HTML = document.getElementById('score--0');
 const score1HTML = document.getElementById('score--1');
 const currentScore0HTML = document.getElementById('current--0');
@@ -17,7 +19,12 @@ let win = false;
 
 // Function to reset all the Var and HTML
 const resetAll = function () {
-  win = false;
+  if (win) {
+    document
+      .querySelector(`.player--winner`)
+      .classList.remove('player--winner');
+    win = false;
+  }
   scores = [0, 0];
   score0HTML.textContent = scores[0];
   score1HTML.textContent = scores[1];
@@ -26,28 +33,71 @@ const resetAll = function () {
 
 // Function to Switch Player
 const switchPlayer = function () {
+  currentScore = 0;
+  document.getElementById(`current--${playerActiv}`).textContent = currentScore;
   playerActiv = playerActiv === 0 ? 1 : 0;
+  player0HTML.classList.toggle('player--active');
+  player1HTML.classList.toggle('player--active');
+};
+
+//Function to hold the Score
+const holdScore = function () {
+  if (!win) {
+    scores[playerActiv] += currentScore;
+    document.getElementById(`score--${playerActiv}`).textContent =
+      scores[playerActiv];
+
+    if (scores[playerActiv] >= 20) {
+      wonGame();
+    } else {
+      switchPlayer();
+    }
+  }
+};
+
+//Function to Reset the Game
+const resetGame = function () {
+  currentScore = 0;
+  document.getElementById(`current--${playerActiv}`).textContent = currentScore;
+  playerActiv = 0;
+  player0HTML.classList.add('player--active');
+  player1HTML.classList.remove('player--active');
+
+  resetAll();
+};
+
+//Function Won Player
+const wonGame = function () {
+  win = true;
+  document
+    .querySelector(`.player--${playerActiv}`)
+    .classList.add('player--winner');
+  document
+    .querySelector(`.player--${playerActiv}`)
+    .classList.remove('player--active');
+  diceHTML.classList.add('hidden');
 };
 
 // Roling the Dice function
 const roleDice = function () {
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  diceHTML.classList.remove('hidden');
-  diceHTML.src = `dices/dice-${dice}.png`;
-  if (dice !== 1) {
-    currentScore += dice;
-    document.getElementById(`current--${playerActiv}`).textContent =
-      currentScore;
-  } else {
-    currentScore = 0;
-    document.getElementById(`current--${playerActiv}`).textContent =
-      currentScore;
-    switchPlayer();
+  if (!win) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    diceHTML.classList.remove('hidden');
+    diceHTML.src = `dices/dice-${dice}.png`;
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${playerActiv}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
   }
 };
 
-//Add Events to the Button
-
-btnNewRoll.addEventListener('click', roleDice);
-
+//Game Start
 resetAll();
+
+//Add Events to the Button
+btnNewRoll.addEventListener('click', roleDice);
+btnHold.addEventListener('click', holdScore);
+btnNewGame.addEventListener('click', resetGame);
